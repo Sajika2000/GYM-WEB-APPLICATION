40f5fc65e4d6a6ef4b0abd5528b2db7cbd0df2b0
Sajika2000 <95673193+Sajika2000@users.noreply.github.com>
2024-08-07 19:05:02 +0530

    create project , hero ,navbar , gallery and pricing pages

---
+++ frontend/src/component/BMICalculator.jsx
@@ -0,0 +1,11 @@
+
+
+const BMICalculator = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BMICalculator
---
+++ frontend/src/component/Navbar.jsx
@@ -0,0 +1,17 @@
+
+
+const Navbar = () => {
+  return (
+    <>
+    <header>
+      <p>ELITE EDGE FITNESS</p>
+
+    </header>
+    
+    
+    </>
+    
+  )
+}
+
+export default Navbar
